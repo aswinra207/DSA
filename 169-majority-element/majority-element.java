619310009835ada24e05b6513c6041d6024102a1
Aswin 03 <aswinra207@gmail.com>
2026-08-10 20:48:08 +0530

    Time: 6 ms (52.12%) | Memory: 55.8 MB (18.39%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,17 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
         int n = nums.length;
-        for(int i:nums){
-            int count=1;
-            for(int j=1;j<nums.length;j++){
-                if(i==nums[j]){
-                    count++;
-                }
-            }
-            if(count>n/2){
-                return i;
-            }
-        }
-        return -1;
+        return nums[n/2];
     }
 }
