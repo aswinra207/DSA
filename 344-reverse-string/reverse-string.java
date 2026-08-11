14c5fd202ea6db89176b72cb47513da2a3ff53ec
Aswin 03 <aswinra207@gmail.com>
2026-08-11 11:30:52 +0530

    Time: 1 ms (23.16%) | Memory: 48.4 MB (21.62%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,7 +3,7 @@ class Solution {
         int left=0;
         int right=s.length-1;
         while(left<=right){
-            char temp=s[right];
+            char temp = s[right];
             s[right]=s[left];
             s[left]=temp;
             left++;
