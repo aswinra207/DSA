c586b96465678e62835eedb08466e3973c7ea9c0
Aswin 03 <aswinra207@gmail.com>
2026-08-13 20:55:35 +0530

    Time: 5 ms (82.94%) | Memory: 77.3 MB (69.21%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,18 +1,22 @@
 class Solution {
     public int maxArea(int[] h) {
         int l=0;
-        int r =h.length-1;
-        int res=0;
+        int r=h.length-1;
+        int maxyy=0;
         while(l<r){
-            int wc=Math.min(h[l],h[r])*(r-l);
-            res=Math.max(wc,res);
-
-            if (h[l]>h[r]){
-                r--;
-            }else{
+            int res=Math.min(h[l],h[r])*(r-l);
+            maxyy=Math.max(maxyy,res);
+            if(h[l]<h[r]){
                 l++;
+            }else{
+                r--;
             }
         }
-        return res;
+        return maxyy;
+
+
+        
+
+        
     }
 }
