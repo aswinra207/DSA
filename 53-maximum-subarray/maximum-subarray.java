0cda881f2d8d259b6dc4f74964ff3526a8c2ce61
Aswin 03 <aswinra207@gmail.com>
2026-08-27 20:33:44 +0530

    Time: 1 ms (99.95%) | Memory: 80.8 MB (5.75%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -2,18 +2,12 @@ class Solution {
     public int maxSubArray(int[] nums) {
         int cs=0;
         int ms=nums[0];
-
-        for(int i: nums){
+        for(int i : nums){
             cs+=i;
-
-            if(cs>ms){
-                ms=cs;
-            }
-            if(cs<0){
-                cs=0;
-            }
+        
+            if(cs>ms) ms=cs;
+            if(cs<0) cs=0;
         }
         return ms;
-        
     }
 }
