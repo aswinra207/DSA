2086b9f6cb021032ba045030e9e3f228347ff13d
Aswin 03 <aswinra207@gmail.com>
2026-09-01 10:58:48 +0530

    Time: 5 ms (82.18%) | Memory: 45.8 MB (87.09%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-
+        if(x<0) return false;
         int copy=x;
         int rev=0;
-
-        while (x>0){
-            rev=(rev*10)+(x%10);
+        while(x>0){
+            rev=rev*10+x%10;
             x/=10;
         }
-
-        if (copy==rev){
+        if(copy==rev){
             return true;
         }
-    return false; 
+    return false;
+        
     }
 }
