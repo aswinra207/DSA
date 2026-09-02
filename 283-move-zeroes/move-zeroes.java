39f5aff22c9b627fd28e90fda84d49854bbf1706
Aswin 03 <aswinra207@gmail.com>
2026-09-02 15:23:14 +0530

    Time: 2 ms (91.84%) | Memory: 47.7 MB (76.42%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,13 +3,13 @@ class Solution {
         int l=0;
         for(int r=0;r<nums.length;r++){
             if(nums[r]!=0){
-                int temp = nums[r];
+                int temp=nums[r];
                 nums[r]=nums[l];
                 nums[l]=temp;
                 l++;
             }
         }
-
+        
         
     }
 }
