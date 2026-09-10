ef09ec7af98dc6268f96f50c2401ee2865374792
Aswin 03 <aswinra207@gmail.com>
2026-09-10 11:49:56 +0530

    Time: 1 ms (99.80%) | Memory: 43.4 MB (8.35%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,27 +1,13 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        if(s.length()==0){
-            return t.charAt(0);
+        int sum=0;
+        for(int i : s.toCharArray()){
+            sum-=i;
         }
-        char[] ch=s.toCharArray();
-        char[] ch1=t.toCharArray();
-
-        Arrays.sort(ch);
-        Arrays.sort(ch1);
-
-        int i=0;
-        int j=0;
-
-        while(i<ch.length && j<ch1.length){
-            if (ch[i]==ch1[j]){
-                i++;
-                j++;
-            }else
-            {
-            return ch1[j];
-            }
+        for(int j : t.toCharArray()){
+            sum+=j;
         }
-        return ch1[ch1.length - 1];
-
+        return (char) sum;
+        
     }
 }
