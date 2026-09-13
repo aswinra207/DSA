ca9115f53a98eedc0b5d2ba0439871d028a76ff0
Aswin 03 <aswinra207@gmail.com>
2026-09-13 12:13:20 +0530

    Time: 5 ms (14.00%) | Memory: 44.7 MB (91.07%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            res[i]=-1;
+            boolean foundtarg=false;
+            for(int j=0;j<nums2.length;j++){
+                if(nums1[i]==nums2[j]){
+                    foundtarg=true;
+                   
+                }
+                if (foundtarg && nums2[j] > nums1[i]) {
+                    res[i] = nums2[j];
+                    break; 
+                }
+
+            }
+            
+        }
+        return res;
+        
+        
+    }
+}
